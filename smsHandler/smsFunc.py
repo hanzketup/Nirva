f4e11cc9ab2e149bd2a5a3de2a1f22a53e92f441
Hannes hermansson <hanneshermansson0000@gmail.com>
2017-11-12 17:18:03 +0100

    fixed bug making ansers only register ones

---
+++ smsHandler/smsFunc.py
@@ -0,0 +1,17 @@
+from .lang import langresolver as lr
+
+def smsSorter(sms):  #handle and sort valid sms requests
+
+    if sms.is_user:
+        kw = sms.kw
+        if kw[0] == '*':
+            Handlecommand(sms)
+
+        else:
+            return sms.respond(lr.msg('nf', sms.lang))
+    else:
+        return sms.respond(lr.msg('nu', sms.lang))
+
+
+def Handlecommand(sms):
+    pass
